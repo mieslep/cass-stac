71da557df81d6be6c49691bcfe4ae83f22cc4913
Phil Miesle <phil.miesle@datastax.com>
2024-05-24 16:03:42 +0100

    add GeoPartition

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import com.uber.h3core.H3Core;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+      H3Core h3 = H3Core.newInstance();
+      double lat = 37.7955;
+      double lng = -122.3937;
+      int res = 10;
+
+      long cell = h3.latLngToCell(lat, lng, res);
+      String hexString = String.format("%016x", cell);
+
+      System.out.println("Hello World! " + hexString);
+
     }
 }
