cfa3e0477026da9ddebe063500926b2d35f8ba58
Mouna Tayehi <mounaTay@users.noreply.github.com>
2024-07-24 18:45:37 +0200

    Revert "Feature/documentation"

---
+++ src/test/main/java/com/datastax/oss/cass_stac/dto/AssetDto.java
@@ -0,0 +1,16 @@
+package com.datastax.oss.cass_stac.dto;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class AssetDto {
+	private String href;
+	private String title;
+	private String description;
+	private String type;
+	private List<String> roles;
+}
